f4bc5912b391f277f753d4a88a6892b825b8c53c
Peter Kwan <peterkwkwan@gmail.com>
2022-06-25 22:20:53 +0800

    feat: address and metamask connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { Content } from './components/Content';
-import { Navbar } from './components/Navbar';
-import { defaultTheme } from './styles/global';
+import { Content } from './container/Content';
+import { Navbar } from './container/Navbar';
+import { defaultTheme, GlobalStyle } from './styles/global';
 
 export default function App() {
+    const [connectedMetamask, setConnectedMetamask] = useState(false);
+    const [accountAddress, setAccountAddress] = useState('');
+
+    const handleAddressChange = (address: string) => {
+        setAccountAddress(address);
+    };
+
+    useEffect(() => {
+        const { ethereum } = window;
+        const checkIfConnected = async () => {
+            setConnectedMetamask(!!ethereum);
+            if (ethereum) handleAddressChange(ethereum.selectedAddress);
+        };
+        checkIfConnected();
+    }, []);
+
     return (
         <ThemeProvider theme={defaultTheme}>
+            <GlobalStyle />
             <Navbar />
-            <Content />
+            <Content
+                connectedMetamask={connectedMetamask}
+                handleAddressChange={handleAddressChange}
+                accountAddress={accountAddress}
+            />
         </ThemeProvider>
     );
 }
