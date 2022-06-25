75859305c1c8e388832d6a3c3862045cecb74209
Peter Kwan <peterkwkwan@gmail.com>
2022-06-25 23:54:16 +0800

    feat: adjust initial loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
 import { ThemeProvider } from 'styled-components';
 import { Content } from './container/Content';
 import { Navbar } from './container/Navbar';
@@ -12,14 +13,36 @@ export default function App() {
         setAccountAddress(address);
     };
 
-    useEffect(() => {
+    const getDetails = async () => {
+        try {
+            const { ethereum } = window;
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
+
+            const addr = await signer.getAddress();
+
+            setAccountAddress(addr.toString());
+        } catch (error) {
+            setConnectedMetamask(false);
+        }
+    };
+
+    const checkIfConnected = async () => {
         const { ethereum } = window;
-        const checkIfConnected = async () => {
-            setConnectedMetamask(!!ethereum);
-            if (ethereum) handleAddressChange(ethereum.selectedAddress);
-        };
+
+        if (ethereum) {
+            setConnectedMetamask(true);
+            getDetails();
+        }
+    };
+
+    useEffect(() => {
         checkIfConnected();
     }, []);
+
+    window.ethereum.on('accountsChanged', () => {
+        getDetails();
+    });
 
     return (
         <ThemeProvider theme={defaultTheme}>
