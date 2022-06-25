9d8bbb160b082217384e1f2d1f306a14154ee5b2
Peter Kwan <peterkwkwan@gmail.com>
2022-06-25 11:23:46 +0800

    feat: add global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    font-family: Monaco, "Courier New", monospace;
+    padding: 0;
+    margin: 0;
+  }
+`;
