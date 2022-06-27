b2faf8dc505029858336e11b803e08a7a7b1d025
Peter Kwan <peterkwkwan@gmail.com>
2022-06-27 17:37:18 +0800

    feat: add hide/show address button

--- src/components/authorized/AddressNumber.tsx
+++ src/components/authorized/AddressNumber.tsx
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { ReactComponent as Eye } from '../../assets/eye.svg';
+import { ReactComponent as Hidden } from '../../assets/hidden.svg';
 
 interface Props {
     accountAddress: string;
 }
 
 const Container = styled.div`
-    color: ${(props) => props.theme.palette.text.main};
+    color: ${(props) => props.theme.palette.text.light};
 `;
 
 const Address = styled.h4`
     display: inline-block;
-    color: ${(props) => props.theme.palette.text.light};
+    color: ${(props) => props.theme.palette.text.main};
+`;
+
+const EyeIcon = styled(Eye)`
+    height: 20px;
+`;
+
+const HiddenIcon = styled(Hidden)`
+    height: 20px;
+`;
+
+const Button = styled.button`
+    border: none;
+    cursor: pointer;
+    background: inherit;
 `;
 export const AccountNumber = ({ accountAddress }: Props) => {
+    const [hidden, setHidden] = useState(true);
+
+    const handleClick = () => {
+        setHidden((prevState) => !prevState);
+    };
     return (
         <Container>
-            Address: <Address>{accountAddress}</Address>
+            Address:{' '}
+            <Address>
+                {hidden
+                    ? `${accountAddress.substring(0, 6)}...`
+                    : accountAddress}
+                <Button type="button" onClick={handleClick}>
+                    {hidden ? <EyeIcon /> : <HiddenIcon />}
+                </Button>
+            </Address>
         </Container>
     );
 };
